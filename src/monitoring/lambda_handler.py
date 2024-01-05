5be9e9b3ee928981447e716a0a868e8178a5a985
zaki3mymy <zaki.last@gmail.com>
2024-01-05 14:45:45 +0000

    implements monitoring

---
+++ src/monitoring/lambda_handler.py
@@ -0,0 +1,101 @@
+import json
+import os
+import urllib.parse
+import urllib.request
+from datetime import datetime, timedelta, timezone
+from logging import getLogger
+
+import boto3
+
+if os.getenv("LOGLEVEL"):
+    log_level = os.getenv("LOGLEVEL")
+else:
+    log_level = "INFO"
+logger = getLogger(__name__)
+logger.setLevel(log_level)
+
+ENDPOINT_ROOT = "https://api.notion.com/v1"
+
+
+def _build_filter_conditions():
+    now = datetime.now(timezone.utc)
+    dt_end = now.replace(second=0, microsecond=0)
+    interval = int(os.environ["INTERVAL_MINUTES"])
+    dt_start = dt_end - timedelta(minutes=interval)
+
+    cond = {
+        "and": [
+            {
+                "timestamp": "last_edited_time",
+                "last_edited_time": {
+                    "after": dt_start.isoformat(),
+                },
+            },
+            {
+                "timestamp": "last_edited_time",
+                "last_edited_time": {
+                    "on_or_before": dt_end.isoformat(),
+                },
+            },
+        ]
+    }
+    return cond
+
+
+def query_database(database_id, filter_conditions):
+    logger.debug("query_database filter conditions: %s", filter_conditions)
+
+    url = f"{ENDPOINT_ROOT}/databases/{database_id}/query"
+    logger.debug("query_database url: %s", url)
+
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    headers = {
+        "Authorization": f"Bearer {SECRET_KEY}",
+        "Content-Type": "application/json",
+        "Notion-Version": "2022-06-28",
+    }
+
+    results = []
+    next_cursor = ""
+    has_more = True
+    while has_more:
+        body = {
+            "filter": filter_conditions,
+            "page_size": 100,
+        }
+        if next_cursor:
+            body["start_cursor"] = next_cursor
+
+        # "Add connect" is required in the Notion database settings
+        req = urllib.request.Request(url, json.dumps(body).encode(), headers)
+        with urllib.request.urlopen(req) as res:
+            body = json.load(res)
+
+        results += body["results"]
+
+        next_cursor = body["next_cursor"]
+        has_more = body["has_more"]
+
+    return results
+
+
+def lambda_function(event, context):
+    logger.info("event: %s", event)
+    database_id = event["DATABASE_ID"]
+    lambda_name = os.environ["LAMBDA_NAME_WEBHOOKS"]
+
+    filter_conditions = _build_filter_conditions()
+    results = query_database(database_id, filter_conditions)
+    logger.info("pages count: %s", len(results))
+
+    client = boto3.client("lambda")
+    for r in results:
+        id_ = r["id"]
+        logger.info("page id: %s", id_)
+        logger.debug("page: %s", r)
+
+        client.invoke(
+            FunctionName=lambda_name,
+            InvocationType="Event",
+            Payload=json.dumps(r),
+        )
---
+++ tests/monitoring/test_monitoring.py
@@ -0,0 +1,114 @@
+import json
+
+import pytest
+from freezegun import freeze_time
+from pytest_mock.plugin import MockerFixture
+
+from monitoring.lambda_handler import lambda_function
+
+LAMBDA_NAME_WEBHOOKS = "webhooks-lambda"
+
+
+@pytest.fixture(autouse=True)
+def setenv(monkeypatch):
+    monkeypatch.setenv(
+        "SECRET_KEY", "secret_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    )
+    monkeypatch.setenv("INTERVAL_MINUTES", "1")
+    monkeypatch.setenv("LAMBDA_NAME_WEBHOOKS", LAMBDA_NAME_WEBHOOKS)
+
+
+@pytest.fixture()
+def mock_lambda_client(monkeypatch, mocker: MockerFixture):
+    monkeypatch.setenv("AWS_DEFAULT_REGION", "us-east-1")
+
+    # Test with a simple Mock.
+    # mock_lambda(from moto) is too long time when it do invoke.
+    mock_client = mocker.MagicMock()
+    mocker.patch("boto3.client", return_value=mock_client)
+
+    return mock_client
+
+
+@freeze_time("2024-01-05T03:58:00Z")
+def test_monitoring(mocker, mock_lambda_client):
+    # prepare
+    # mock Notion API
+    body = {
+        "results": [
+            {
+                "object": "page",
+                "id": "d2b8393e-2817-4009-8311-57f9dcac0185",
+                "created_time": "2024-01-02T08:11:00.000Z",
+                "last_edited_time": "2024-01-05T03:58:00.000Z",
+                "created_by": {
+                    "object": "user",
+                    "id": "ca7192dd-8e7a-4ccc-8faa-3e230d28778d",
+                },
+                "last_edited_by": {
+                    "object": "user",
+                    "id": "ca7192dd-8e7a-4ccc-8faa-3e230d28778d",
+                },
+                "cover": None,
+                "icon": {"type": "emoji", "emoji": ""},
+                "parent": {
+                    "type": "database_id",
+                    "database_id": "36142bf2-4820-4514-8891-12bcb8b8cf2",
+                },
+                "archive": False,
+                "properties": {
+                    "Category": {
+                        "id": "%3AaT",
+                        "typ": "multi_select",
+                        "multi_select": [],
+                    },
+                    "Name": {
+                        "id": "title",
+                        "type": "title",
+                        "title": [
+                            {
+                                "type": "text",
+                                "text": {
+                                    "content": "bbb",
+                                    "lin": None,
+                                },
+                                "annotations": {
+                                    "bold": False,
+                                    "italic": False,
+                                    "strikethrough": False,
+                                    "underline": False,
+                                    "code": False,
+                                    "color": "default",
+                                },
+                                "plain_text": "bbb",
+                                "href": None,
+                            },
+                        ],
+                    },
+                },
+                "url": "https://www.notion.so/bbb-xxxxxxxx",
+                "public_url": None,
+            }
+        ],
+        "next_cursor": None,
+        "has_more": False,
+    }
+    mock_read = mocker.MagicMock(return_value=json.dumps(body))
+    mock_res = mocker.MagicMock(read=mock_read)
+    mock_urlopen = mocker.MagicMock()
+    mock_urlopen.return_value.__enter__.return_value = mock_res
+    mocker.patch("urllib.request.urlopen", mock_urlopen)
+
+    # execute
+    event = {
+        "DATABASE_ID": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    }
+    lambda_function(event, {})
+
+    # verify
+    exp = json.dumps(body["results"][0])
+    mock_lambda_client.invoke.assert_called_once_with(
+        FunctionName=LAMBDA_NAME_WEBHOOKS,
+        InvocationType="Event",
+        Payload=exp,
+    )
