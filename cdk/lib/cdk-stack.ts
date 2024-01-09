3e57c32d035d5b7b3d04a780bdcd1470509fa716
zaki3mymy <zaki.last@gmail.com>
2024-01-09 14:49:03 +0000

    fix cdk script

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -21,24 +21,15 @@ export class CdkStack extends cdk.Stack {
       throw new Error("Environmental variable NOTION_SECRET_KEY is not set.");
     }
 
-    // IAM
-    const iamRoleForMonitoring = new iam.Role(this, "iam-role-lambda-monitoring", {
-      roleName: `${props.projectName}-monitoring-lambda-role`,
-      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
-      managedPolicies: [
-        {
-          "managedPolicyArn": "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-        }
-      ]
-    })
-    const iamRoleForWebhooks = new iam.Role(this, "iam-role-lambda-webhooks", {
-      roleName: `${props.projectName}-webhooks-lambda-role`,
-      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
-      managedPolicies: [
-        {
-          "managedPolicyArn": "arn:aws:iam::aws:policy/service-role/AWSLambdaDynamoDBExecutionRole"
-        }
-      ]
+    //////// Common
+    const duration = Math.min(900, props.intervalMinutes * 60);
+
+    // Lambda Layer
+    const lambdaLayer = new lambda.LayerVersion(this, "lambda-layer", {
+      layerVersionName: `${props.projectName}-common-layer`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      code: lambda.Code.fromAsset("../lib/"),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_12]
     })
 
     // DynamoDB
@@ -48,23 +39,36 @@ export class CdkStack extends cdk.Stack {
         name: "id",
         type: dynamodb.AttributeType.STRING,
       },
-      sortKey: {
-        name: "last_edited_time",
-        type: dynamodb.AttributeType.STRING,
-      },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,  // On-demand request
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     })
 
-    // Lambda Layer
-    const lambdaLayer = new lambda.LayerVersion(this, "lambda-layer", {
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      code: lambda.Code.fromAsset("../lib"),
-      compatibleRuntimes: [lambda.Runtime.PYTHON_3_12]
+    //////// Webhooks
+    // IAM
+    const iamPolicyForWebhooks = new iam.Policy(this, "iam-policy-dynamodb", {
+      policyName: `${props.projectName}-dynamodb-policy`,
+      statements: [
+        new iam.PolicyStatement({
+          actions: [
+            "dynamodb:GetItem",
+            "dynamodb:PutItem",
+          ],
+          resources: [dynamodbTable.tableArn],
+        })
+      ]
     })
+    const iamRoleForWebhooks = new iam.Role(this, "iam-role-lambda-webhooks", {
+      roleName: `${props.projectName}-webhooks-lambda-role`,
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      managedPolicies: [
+        {
+          "managedPolicyArn": "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+        }
+      ]
+    })
+    iamRoleForWebhooks.attachInlinePolicy(iamPolicyForWebhooks);
 
-    // Lambda Function
-    const duration = Math.min(900, props.intervalMinutes * 60);
+    // Lambda
     const lambdaWebhooks = new lambda.Function(this, "lambda-webhooks", {
       functionName: `${props.projectName}-webhooks-lambda`,
       runtime: lambda.Runtime.PYTHON_3_12,
@@ -79,6 +83,30 @@ export class CdkStack extends cdk.Stack {
       },
       layers: [lambdaLayer]
     })
+
+    //////// Monitoring
+    // IAM
+    const iamPolicyForInvoking = new iam.Policy(this, "iam-policy-lambda-invoking", {
+      policyName: `${props.projectName}-invoke-policy`,
+      statements: [
+        new iam.PolicyStatement({
+          actions: ["lambda:InvokeFunction"],
+          resources: [lambdaWebhooks.functionArn],
+        })
+      ]
+    })
+    const iamRoleForMonitoring = new iam.Role(this, "iam-role-lambda-monitoring", {
+      roleName: `${props.projectName}-monitoring-lambda-role`,
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      managedPolicies: [
+        {
+          "managedPolicyArn": "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+        }
+      ]
+    });
+    iamRoleForMonitoring.attachInlinePolicy(iamPolicyForInvoking);
+
+    // Lambda
     const lambdaMonitoring = new lambda.Function(this, "lambda-monitoring", {
       functionName: `${props.projectName}-monitoring-lambda`,
       runtime: lambda.Runtime.PYTHON_3_12,
@@ -88,7 +116,7 @@ export class CdkStack extends cdk.Stack {
       role: iamRoleForMonitoring,
       environment: {
         "LOGLEVEL": "INFO",
-        "SECRET_KEY": "secret_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "SECRET_KEY": props.notionSecretKey,
         "INTERVAL_MINUTES": String(props.intervalMinutes),
         "LAMBDA_NAME_WEBHOOKS": lambdaWebhooks.functionName,
       }
